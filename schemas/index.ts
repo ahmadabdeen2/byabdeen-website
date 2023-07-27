6f7de659986ce1c22f7ef87851bcee4b94961998
ahmadabdeen2 <ahmadabdin3@hotmail.com>
2023-07-27 20:15:17 +0300

    added portfolio post

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,6 +1,14 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import project from './project'
-export const schemaTypes = [post, author, category, blockContent, project]
+import blockContent from "./blockContent";
+import category from "./category";
+import post from "./post";
+import author from "./author";
+import project from "./project";
+import portfolioPost from "./portfolioPost";
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  project,
+  portfolioPost,
+];
