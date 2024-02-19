c066c2d54c97225b4b6b21290277d40cebf6b673
ahmadabdeen2 <ahmadabdin3@hotmail.com>
2024-02-19 14:17:02 +0300

    fixed image issue

--- components/BlogComponent.tsx
+++ components/BlogComponent.tsx
@@ -1,35 +1,50 @@
-'use client'
-import { urlFor } from '../lib/urlFor'
-import { Post } from '../typings'
-import { motion } from 'framer-motion'
-import Image from 'next/image'
-import CustomButton from './CustomButton'
-import Link from 'next/link'
+"use client";
+import { urlFor } from "../lib/urlFor";
+import { Post } from "../typings";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+import Link from "next/link";
 
 type Props = {
-    post: Post,
-    index: number
-}
+  post: Post;
+  index: number;
+};
 
-const BlogComponent = ({post, index}: Props) => {
+const BlogComponent = ({ post, index }: Props) => {
   return (
-    <motion.div 
-    initial = {{opacity: 0, y: 50}}
-    whileInView = {{opacity: 1, y: 0}}
-    transition = {{duration: 1}}
-    viewport ={{once:true}}
-
-    className='flex flex-col items-start'>
-        <Link href = {`/blog/${post.slug.current}`}>
-        <div className='flex justify-center items-center mt-4 w-full'>
-        <Image src = {urlFor(post.mainImage).url()} alt = {post.title} width={1000} height={1000} className='hover:brightness-75 transition ease-out duration-300'/>
+    <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+      className="flex flex-col items-start"
+    >
+      <Link href={`/blog/${post.slug.current}`}>
+        <div className="flex justify-center items-center mt-4 w-full">
+          {post.mainImage && (
+            <Image
+              src={urlFor(post.mainImage).url()}
+              alt={post.title}
+              width={1000}
+              height={1000}
+              className="hover:brightness-75 transition ease-out duration-300"
+            />
+          )}
         </div>
-        <div className='flex justify-between w-full items-center mt-4'>
-            <div className='flex justify-center items-center '>
-            <p className='text-secondary text-[3vw] xs:text-[2vw]  font-random font-semibold'> {index > 10 ? '' : '0'}{index}. </p>
-            <h3 className='text-secondary text-[3vw] xs:text-[2vw] font-random font-semibold ml-2'> {post?.title} </h3>
-            </div>
-            <CustomButton
+        <div className="flex justify-between w-full items-center mt-4">
+          <div className="flex justify-center items-center ">
+            <p className="text-secondary text-[3vw] xs:text-[2vw]  font-random font-semibold">
+              {" "}
+              {index > 10 ? "" : "0"}
+              {index}.{" "}
+            </p>
+            <h3 className="text-secondary text-[3vw] xs:text-[2vw] font-random font-semibold ml-2">
+              {" "}
+              {post?.title}{" "}
+            </h3>
+          </div>
+          <CustomButton
             backgroundColor="bg-secondary"
             arrowColor="dark"
             size="w-[4vw] h-[4vw] xs:w-[3vw] xs:h-[3vw] md:w-[2.5vw] md:h-[2.5vw]"
@@ -37,12 +52,10 @@ const BlogComponent = ({post, index}: Props) => {
             arrowSize="w-[1.5vw] xs:w-[1vw] md:w-[0.8vw]"
             cursor="cursor-pointer"
           />
-
-            </div>
-        </Link>
-
+        </div>
+      </Link>
     </motion.div>
-  )
-}
+  );
+};
 
-export default BlogComponent
+export default BlogComponent;
